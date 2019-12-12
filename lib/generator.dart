4c4f75c938b21ced20c7142921e671b91cb861ef
Krzysztof <j.krzysztof13@gmail.com>
2019-12-12 22:43:49 +0100

    better variables names and comments

--- lib/generator.dart
+++ lib/generator.dart
@@ -3,31 +3,31 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class MazeGenerator extends CustomPainter {
-  var dupa;
+  var labirynth;
   var sizee;
 
   List<List<int>> maze() {
-    var w = 1;
-    var h = 1;
+    var c = 1; // column
+    var r = 1; //row
     final size = 29;
     final grid = List<List<int>>.generate(size,
         (i) => List<int>.generate(size, (j) => (j - 1) % 2 | (i - 1) % 2));
-    for (; h < size - 1; h++) {
-      if (h % 2 == 0) {
-        h++;
+    for (; r < size - 1; r++) {
+      if (r % 2 == 0) {
+        r++;
       }
-      for (; w < size - 1; w++) {
-        if (w % 2 == 0) {
-          w++;
+      for (; c < size - 1; c++) {
+        if (c % 2 == 0) {
+          c++;
         }
         Random random = new Random();
         var ran = random.nextInt(2);
 
         print(ran);
         if (ran == 0) /* right wall*/ {
-          if (grid[h][w + 1] == 1) {
-            if (w + 1 < size - 1) /*it wont delete edge line */ {
-              grid[h][w + 1] = 0;
+          if (grid[r][c + 1] == 1) /*is there wall or empty space*/{
+            if (c + 1 < size - 1) /*it wont delete edge line */ {
+              grid[r][c + 1] = 0;
               print("d0psko");
             } else {
               ran = 1;
@@ -35,57 +35,59 @@ class MazeGenerator extends CustomPainter {
           }
         } 
         if (ran == 1) /* top wall*/ {
-          if (grid[h - 1][w] == 1) {
-            if (h - 1 > 0) /*it wont delete edge line */ {
-              grid[h - 1][w] = 0;
+          if (grid[r - 1][c] == 1)/*is there wall or empty space*/ {
+            if (r - 1 > 0) /*it wont delete edge line */ {
+              grid[r - 1][c] = 0;
               print("d0pa");
             } else {
-              if (w + 1 < size - 1) /*it wont delete edge line */ {
-                grid[h][w + 1] = 0;
+              if (c + 1 < size - 1) /*it wont delete edge line */ {
+                grid[r][c + 1] = 0;
                 print("dupa");
               }
             }
           }
         }
       }
-      w = 0;
+      c = 0;
     }
-    dupa = grid;
+    labirynth = grid;
     sizee = size;
   }
 
   @override
-  void paint(Canvas canvas, Size sizeee) {
+  void paint(Canvas canvas, Size size) {
     maze();
-    var w = 0;
-    var h = 0;
+    var c = 0; //column
+    var r = 0; //row
 
-    const l = 12.0;
+    const l = 12.0; //lenght of wall
 
     final Paint paint = Paint();
     paint.color = Colors.black;
     paint.strokeWidth = 3;
 
-    print(dupa);
+    //print(labirynth);
 
-    for (; h < sizee; h++) {
-      for (; w < sizee; w++) {
-        var p11 = Offset(l * w, l * h);
-        var p21 = Offset(l * (w + 1), l * h);
-        if (h % 2 == 0) {
-          if (dupa[h][w] % 2 == 1) {
+    for (; r < sizee; r++) {
+      for (; c < sizee; c++) {
+        //horizontal walls
+        var p11 = Offset(l * c, l * r); //start point of wall
+        var p21 = Offset(l * (c + 1), l * r); //end point od wall
+        if (r % 2 == 0) {
+          if (labirynth[r][c] % 2 == 1) {
             canvas.drawLine(p11, p21, paint);
           }
         }
-        var p12 = Offset(l * (w + 0.5), l * (h - 1));
-        var p22 = Offset(l * (w + 0.5), l * (h + 1));
-        if (h % 2 == 1) {
-          if (dupa[h][w] % 2 == 1) {
+        //vertical walls
+        var p12 = Offset(l * (c + 0.5), l * (r - 1));//start point of wall
+        var p22 = Offset(l * (c + 0.5), l * (r + 1));//end point od wall
+        if (r % 2 == 1) {
+          if (labirynth[r][c] % 2 == 1) {
             canvas.drawLine(p12, p22, paint);
           }
         }
       }
-      w = 0;
+      c = 0;
     }
   }
 
